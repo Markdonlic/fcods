82ace1ac3878577406bb42bd5b8000940d8cfc32
sun <markwilliam01259@outlook.com>
2023-10-08 10:50:43 +0100

    dashboard updated

--- app/a-home/page.jsx
+++ app/a-home/page.jsx
@@ -8,7 +8,7 @@ import {
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-3 bg-gray-100">
+    <div className="w-full flex flex-col justify-center items-center gap-3">
       <div className="bg-red-500 w-full fixed top-0 left-0 h-16 flex items-center justify-between text-white px-4 text-3xl">
         <div className="font-semibold">
           Good Morning, <span>Joe</span>
@@ -21,7 +21,7 @@ const page = () => {
         </div>
       </div>
       <div className="h-16 w-full p-4"></div>
-      <div className="bg-white h-auto w-full flex flex-col justify-center items-center gap-3">
+      <div className="bg-white w-full flex flex-col justify-center items-center gap-3">
         <div className="w-[90%] bg-gradient-to-r from-neutral-700 to-neutral-500 text-white rounded-lg p-4 flex flex-col gap-4 justify-center items-center">
           <div className="flex w-full justify-between items-center">
             <div className="text-xs">Total Balance</div>
--- components/Alert.jsx
+++ components/Alert.jsx
@@ -1,16 +1,18 @@
-import { HiMiniArrowUpRight } from "react-icons/hi2";
+import { HiMiniArrowDownRight, HiMiniArrowUpRight } from "react-icons/hi2";
 export default function Alert(props) {
   return (
     <div className="flex justify-between items-center gap-2">
       <div className="">
-        <HiMiniArrowUpRight className="p-1 bg-red-100 text-red-600 text-3xl rounded-lg" />
+        <HiMiniArrowUpRight className="p-1 bg-red-100 hidden text-red-600 text-2xl rounded-lg" />
+        <HiMiniArrowDownRight className="p-1 bg-green-100 text-green-600 text-2xl rounded-lg" />
       </div>
       <div className="flex flex-col flex-wrap items-start justify-center text-xs">
         <div>Sep 27, 2023 10:58PM</div>
         <div>SMS Notification Charge Sep 2023</div>
       </div>
       <div className="flex flex-col items-end justify-center text-xs">
-        <div className="text-red-500">-NGN53.25</div>
+        <div className="text-red-500 hidden">-NGN53.25</div>
+        <div className="text-green-500">-NGN53.25</div>
         <div>NGN2,300,225.20</div>
       </div>
     </div>
